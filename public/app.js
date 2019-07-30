ed7f39ed200a903647040bf12aa61d874176790a
Mingjie Jiang <jiang@mingjie.info>
2019-07-30 14:49:41 -0700

    Remove link tracking

--- public/app.js
+++ public/app.js
@@ -1,25 +1,14 @@
 $(function () {
 
-    var pageID = unescape(getUrlVars()["p"])
-
     if (window.location.hostname != "localhost") {
 
-        if (window.location != window.parent.location) {
-            if (pageID == "undefined" || pageID == "") {
-                $("body").html("<div style=\"text-align: center\"><a href=\"https://www.notion.so/How-the-Referral-Box-Works-69fc3ec2882d44cf99870ca710522e5b\" target=\"_blank\">Misconfigured embed detected. Click here to be redirected.</a></div>")
-                return
-            }
-        } else {
+        if (window.location == window.parent.location) {
             window.location.replace("https://www.notion.so/hackclub/How-the-Referral-Box-Works-69fc3ec2882d44cf99870ca710522e5b")
         }
 
-    } else {
-        pageID = "localhost"
     }
     
     $("body").removeClass("hidden")
-    $("#pageID").html(pageID)
-    
 
     $('#appreciate').submit(function (e) {
         $("#button-spinner").removeClass("hidden")
@@ -38,8 +27,7 @@ $(function () {
             url: '/chopchop',
             data: {
                 source: sourceVal,
-                username: usernameVal,
-                page: "https://www.notion.so/hackclub/" + pageID
+                username: usernameVal
             }
         }).done(function (res) {
             if (res.success) {
@@ -49,11 +37,3 @@ $(function () {
         });
     });
 })
-
-function getUrlVars() {
-    var vars = {}
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-        vars[key] = value
-    })
-    return vars
-}
